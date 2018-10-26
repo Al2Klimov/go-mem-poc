52c7692ac41952562672074f5fe2098972e01c81
Alexander A. Klimov <alexander.klimov@netways.de>
2018-10-26 12:41:29 +0200

    Proof Go's memory leak

---
+++ main.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"os"
+	"runtime"
+	"runtime/debug"
+	"time"
+)
+
+const bufLen = 1024 * 1024 * 1024
+
+func main() {
+	go logStats()
+
+	fmt.Println("Sleeping 10s...")
+	time.Sleep(10 * time.Second)
+
+	fmt.Println("Allocating 1GiB of nonsense...")
+	fmt.Printf("Nonsense = %d\n", getNonsense())
+	fmt.Println("Deallocated nonsense.")
+
+	fmt.Println("Sleeping 10s...")
+	time.Sleep(10 * time.Second)
+
+	fmt.Println("runtime.GC()...")
+	runtime.GC()
+
+	fmt.Println("Sleeping 10s...")
+	time.Sleep(10 * time.Second)
+
+	fmt.Println("debug.FreeOSMemory()...")
+	debug.FreeOSMemory()
+
+	fmt.Println("Sleeping 1h...")
+	time.Sleep(time.Hour)
+}
+
+func logStats() {
+	for {
+		time.Sleep(time.Second / 2)
+
+		var mst runtime.MemStats
+		runtime.ReadMemStats(&mst)
+
+		fmt.Fprintf(os.Stderr, "MEM = %d @ %s\n", mst.Sys, time.Now().Format(time.RFC3339))
+	}
+}
+
+func getNonsense() (sum uint64) {
+	buf := make([]byte, bufLen)
+
+	rand.Read(buf)
+
+	{
+		var tmp byte
+
+		for i := 0; i < bufLen; i += 2 {
+			tmp = buf[i]
+			buf[i] = buf[i+1]
+			buf[i+1] = tmp
+		}
+	}
+
+	for _, b := range buf {
+		sum += uint64(b)
+	}
+
+	return
+}
